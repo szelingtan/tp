92c333a37d674e4cbdcab91dd252d3cbaebcf2e6
Bryce-3D <bryceainsleysanchez@gmail.com>
2025-03-07 13:12:26 +0800

    Rename PersonListPanel to PatientListPanel

--- src/main/java/seedu/address/ui/PatientListPanel.java
+++ src/main/java/seedu/address/ui/PatientListPanel.java
@@ -13,9 +13,9 @@ import seedu.address.model.person.Patient;
 /**
  * Panel containing the list of persons.
  */
-public class PersonListPanel extends UiPart<Region> {
+public class PatientListPanel extends UiPart<Region> {
     private static final String FXML = "PersonListPanel.fxml";
-    private final Logger logger = LogsCenter.getLogger(PersonListPanel.class);
+    private final Logger logger = LogsCenter.getLogger(PatientListPanel.class);
 
     @FXML
     private ListView<Patient> personListView;
@@ -23,7 +23,7 @@ public class PersonListPanel extends UiPart<Region> {
     /**
      * Creates a {@code PersonListPanel} with the given {@code ObservableList}.
      */
-    public PersonListPanel(ObservableList<Patient> patientList) {
+    public PatientListPanel(ObservableList<Patient> patientList) {
         super(FXML);
         personListView.setItems(patientList);
         personListView.setCellFactory(listView -> new PersonListViewCell());
