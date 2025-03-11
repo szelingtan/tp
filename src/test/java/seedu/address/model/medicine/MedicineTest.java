efb5dd6a66f4e3c170d2ea92d06f7bd30e1bb620
Luoq1-Xu <gancelot36@gmail.com>
2025-03-11 21:52:58 +0800

    Add medicine test

--- src/test/java/seedu/address/model/medicine/MedicineTest.java
+++ src/test/java/seedu/address/model/medicine/MedicineTest.java
@@ -1,4 +1,28 @@
-package seedu.address.model.Medicine;
+package seedu.address.model.medicine;
+
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.medicine.Medicine;
 
 public class MedicineTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Medicine(null));
+    }
+
+    @Test
+    public void constructor_invalidTagName_throwsIllegalArgumentException() {
+        String invalidTagName = "";
+        assertThrows(IllegalArgumentException.class, () -> new Medicine(invalidTagName));
+    }
+
+    @Test
+    public void isValidTagName() {
+        // null tag name
+        assertThrows(NullPointerException.class, () -> Medicine.isValidMedName(null));
+    }
+
 }
