d26ba1d5cf2df673b6dffb9bbcc2a0a0f5497b6b
Bryce-3D <bryceainsleysanchez@gmail.com>
2025-03-13 11:16:22 +0800

    Initialize test files

---
+++ src/test/java/seedu/address/logic/commands/DelLastVisitCommandTest.java
@@ -0,0 +1,10 @@
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.Test;
+
+public class DelLastVisitCommandTest {
+    @Test
+    public void function_input_expected() {
+        // TODO
+    }
+}
---
+++ src/test/java/seedu/address/logic/parser/DelLastVisitCommandParserTest.java
@@ -0,0 +1,10 @@
+package seedu.address.logic.parser;
+
+import org.junit.jupiter.api.Test;
+
+public class DelLastVisitCommandParserTest {
+    @Test
+    public void function_input_expected() {
+        // TODO
+    }
+}
