a46e16c10ab610525a3c4deca9feaa2f4e806c08
Bryce-3D <bryceainsleysanchez@gmail.com>
2025-03-13 16:25:51 +0800

    Add tests for DelLastVisitCommandParserTest

--- src/test/java/seedu/address/logic/parser/DelLastVisitCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DelLastVisitCommandParserTest.java
@@ -1,10 +1,33 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+
 import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.DelLastVisitCommand;
 
 public class DelLastVisitCommandParserTest {
     @Test
-    public void function_input_expected() {
-        // TODO
+    public void parse_validArg_success() {
+        // Copied from DeleteCommandParserTest.java
+        assertParseSuccess(
+                new DelLastVisitCommandParser(),
+                "1",
+                new DelLastVisitCommand(INDEX_FIRST_PERSON)
+        );
+    }
+
+    @Test
+    public void parse_invArg_error() {
+        // Copied from DeleteCommandParserTest.java
+        assertParseFailure(
+                new DelLastVisitCommandParser(),
+                "Homura",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                        DelLastVisitCommand.MESSAGE_USAGE)
+        );
     }
 }
