7aa351f1ad000862886fc43cdab11673d842bfd7
Bryce-3D <bryceainsleysanchez@gmail.com>
2025-03-13 10:57:50 +0800

    Add some javadocs

--- src/main/java/seedu/address/logic/commands/DelLastVisitCommand.java
+++ src/main/java/seedu/address/logic/commands/DelLastVisitCommand.java
@@ -24,15 +24,33 @@ public class DelLastVisitCommand extends Command {
 
     private final Index index;
 
+    /**
+     * Creates a {@code DelLastVisitCommand}.
+     *
+     * @param ind The index of the patient to modify.
+     */
     public DelLastVisitCommand(Index ind) {
         index = ind;
     }
 
+    /**
+     * Generates the msg to send upon successful deletion.
+     *
+     * @param patient The newly modified patient.
+     * @return The msg to send upon successful deletion.
+     */
     public String generateSuccessMessage(Patient patient) {
         return "Last visit info successfully deleted from "
                 + patient.getName();
     }
 
+    /**
+     * Executes the stored `delLastVisit` cmd.
+     *
+     * @param model {@code Model} which the command should operate on.
+     * @return The {@code CommandResult}.
+     * @throws CommandException If the index is out of range.
+     */
     @Override
     public CommandResult execute(Model model) throws CommandException {
         List<Patient> lastShownList = model.getFilteredPersonList();
