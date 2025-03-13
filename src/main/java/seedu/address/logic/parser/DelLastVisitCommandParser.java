82510c08e043e1cc9dac1a793f01abf0f82ef5c5
Bryce-3D <bryceainsleysanchez@gmail.com>
2025-03-13 10:18:21 +0800

    Implement DelLastVisitCommandParser.java

---
+++ src/main/java/seedu/address/logic/parser/DelLastVisitCommandParser.java
@@ -0,0 +1,32 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.DelLastVisitCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class DelLastVisitCommandParser
+        implements Parser<DelLastVisitCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the
+     * AddCommand and returns an AddCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected
+     * format
+     */
+    public DelLastVisitCommand parse(String args) throws ParseException {
+        // Format copied from DeleteCommandParser.java
+        try {
+            Index index = ParserUtil.parseIndex(args);
+            return new DelLastVisitCommand(index);
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(
+                            MESSAGE_INVALID_COMMAND_FORMAT,
+                            DelLastVisitCommand.MESSAGE_USAGE
+                    ),
+                    pe
+            );
+        }
+    }
+}
