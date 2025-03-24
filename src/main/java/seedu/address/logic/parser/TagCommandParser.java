4676cc8d91e485c549df214484ad87d95c8abec4
Bryce-3D <bryceainsleysanchez@gmail.com>
2025-03-24 16:56:14 +0800

    Initialize TagCommandParser.java

---
+++ src/main/java/seedu/address/logic/parser/TagCommandParser.java
@@ -0,0 +1,22 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.DelLastVisitCommand;
+import seedu.address.logic.commands.TagCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new TagCommand
+ */
+public class TagCommandParser implements Parser<TagCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the
+     * {@code TagCommand} and returns a {@code TagCommand} object for
+     * execution.
+     *
+     * @throws ParseException if the user input does not conform to the
+     *     expected format
+     */
+    public TagCommand parse(String args) throws ParseException {
+        return null; // TODO
+    }
+}
