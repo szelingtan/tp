42f7f054689d7691fa1aa845d81b3b4d5f191217
Bryce-3D <bryceainsleysanchez@gmail.com>
2025-03-25 19:20:42 +0800

    Initialize test files

---
+++ src/test/java/seedu/address/logic/commands/TagCommandTest.java
@@ -0,0 +1,13 @@
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.patient.Patient;
+import seedu.address.testutil.PatientBuilder;
+import seedu.address.testutil.TypicalPatients;
+
+public class TagCommandTest {
+    @Test
+    public void execute_onePatient_normal() {
+    }
+}
