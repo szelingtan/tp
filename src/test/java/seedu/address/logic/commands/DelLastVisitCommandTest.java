c290e9f3e8688d02160701614c9465b48b741539
Bryce-3D <bryceainsleysanchez@gmail.com>
2025-03-25 19:15:46 +0800

    Remove unused commented out code

--- src/test/java/seedu/address/logic/commands/DelLastVisitCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DelLastVisitCommandTest.java
@@ -25,7 +25,6 @@ public class DelLastVisitCommandTest {
                 .withLastVisit("Meduka become Meguca")
                 .build();
         Patient p1 = new PatientBuilder(p0)
-                // .withLastVisit(DelLastVisitCommand.NONE)
                 .withLastVisit(null)
                 .build();
 
