f5ab6626e3d0d28812229d5bf9750e91dec328cf
Bryce-3D <bryceainsleysanchez@gmail.com>
2025-03-28 15:33:23 +0800

    Fix typo in add cmd usage msg

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -34,7 +34,7 @@ public class AddCommand extends Command {
             + PREFIX_PHONE + "98765432 "
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
-            + PREFIX_DATE + "2025-01-01, low on xanax "
+            + PREFIX_DATE + "2025-01-01 "
             + PREFIX_TAG + "friends "
             + PREFIX_TAG + "owesMoney";
 
