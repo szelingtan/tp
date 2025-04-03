f9df23c2a5e4be74a42bd08e268941a273fdddc0
Bryce-3D <bryceainsleysanchez@gmail.com>
2025-04-03 10:53:16 +0800

    Fix codestyle

--- src/main/java/seedu/address/logic/parser/UntagCommandParser.java
+++ src/main/java/seedu/address/logic/parser/UntagCommandParser.java
@@ -8,7 +8,6 @@ import java.util.HashSet;
 import java.util.List;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.TagCommand;
 import seedu.address.logic.commands.UntagCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.tag.Tag;
