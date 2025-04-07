5d13623c0d72559bdee2b41c17d6bad868c51cd8
Eunice <e1156977@u.nus.edu>
2025-04-07 22:23:46 +0800

    Fix bugs with edit command

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -3,9 +3,12 @@ package seedu.address.logic.parser;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MEDICINE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -31,7 +34,8 @@ public class EditCommandParser implements Parser<EditCommand> {
     public EditCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS);
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS,
+                        PREFIX_DATE, PREFIX_MEDICINE, PREFIX_TAG);
 
         Index index;
 
@@ -45,6 +49,12 @@ public class EditCommandParser implements Parser<EditCommand> {
 
         EditPatientDescriptor editPatientDescriptor = new EditPatientDescriptor();
 
+        if (argMultimap.getValue(PREFIX_TAG).isPresent() || argMultimap.getValue(PREFIX_MEDICINE).isPresent()) {
+            throw new ParseException(EditCommand.MESSAGE_EDIT_NOT_ALLOWED);
+        }
+        if (argMultimap.getValue(PREFIX_DATE).isPresent()) {
+            throw new ParseException(EditCommand.MESSAGE_EDIT_NOT_ALLOWED);
+        }
         if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
             editPatientDescriptor.setName(ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get()));
         }
